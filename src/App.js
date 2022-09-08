c0161927ac7235c9be0eaebbfdf81673c2e0d359
Faisal <faisalvin15@gmail.com>
2022-09-08 12:09:14 +0700

    create component and install react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import HomePage from "./components/pages/HomePage";
+import AboutPage from "./components/pages/AboutPage";
+import ContactPage from "./components/pages/ContactPage";
+import NavBar from "./components/layout/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <h2>Ums Application</h2>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route  path="/" element={<HomePage />} />
+          <Route  path="/about" element={<AboutPage />} />
+          <Route  path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Penulisan tidak menggunakan es6
+// function HomePage() {
+//   return (
+//     <div>homePage</div>
+//   )
+// }
+
+// Penulisan menggunakan es6
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h2>Home Page</h2>
+    </div>
+  );
+};
+
+export default HomePage;
