f210689d97c2b9c072129cb774a1730e889d4cee
Faisal <faisalvin15@gmail.com>
2022-09-08 13:53:05 +0700

    add content in AboutPage and ContactPage Components

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -3,7 +3,53 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div className="container">
-      <h2>About Page</h2>
+      <h2 className="py-2">About Page</h2>
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Consequat mauris
+        nunc congue nisi vitae suscipit tellus. Quam adipiscing vitae proin
+        sagittis nisl rhoncus mattis. Lorem dolor sed viverra ipsum nunc.
+        Faucibus nisl tincidunt eget nullam non nisi. Neque laoreet suspendisse
+        interdum consectetur libero id. Hac habitasse platea dictumst vestibulum
+        rhoncus est pellentesque elit. Convallis aenean et tortor at. Nibh nisl
+        condimentum id venenatis a condimentum vitae. Eget velit aliquet
+        sagittis id consectetur purus ut faucibus. Ullamcorper velit sed
+        ullamcorper morbi. At volutpat diam ut venenatis tellus in. Mi tempus
+        imperdiet nulla malesuada. Eget nullam non nisi est sit amet.
+      </p>
+
+      <p className="lead">
+        Eros donec ac odio tempor orci. Platea dictumst quisque sagittis purus
+        sit amet volutpat consequat mauris. Ipsum suspendisse ultrices gravida
+        dictum fusce ut. Eleifend mi in nulla posuere sollicitudin aliquam
+        ultrices sagittis. Massa massa ultricies mi quis hendrerit dolor. Proin
+        libero nunc consequat interdum varius. Id diam vel quam elementum
+        pulvinar. At tempor commodo ullamcorper a lacus vestibulum. Pellentesque
+        diam volutpat commodo sed. Leo vel fringilla est ullamcorper eget nulla.
+        Magna etiam tempor orci eu lobortis. Sagittis orci a scelerisque purus
+        semper eget duis at tellus. Arcu cursus euismod quis viverra nibh cras
+        pulvinar. Enim facilisis gravida neque convallis a cras. Volutpat est
+        velit egestas dui id ornare arcu. Nunc faucibus a pellentesque sit amet
+        porttitor eget dolor. Quam id leo in vitae. Fringilla est ullamcorper
+        eget nulla facilisi etiam dignissim diam. Bibendum ut tristique et
+        egestas quis ipsum suspendisse.
+      </p>
+      <p className="lead">
+        Ultricies mi eget mauris pharetra et ultrices neque. Est placerat in
+        egestas erat imperdiet sed. Commodo ullamcorper a lacus vestibulum sed
+        arcu. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper
+        velit. Nam at lectus urna duis. Tincidunt vitae semper quis lectus nulla
+        at. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus
+        pellentesque. Cum sociis natoque penatibus et magnis dis parturient.
+        Aliquam vestibulum morbi blandit cursus risus. Tincidunt ornare massa
+        eget egestas purus viverra accumsan in. Tristique magna sit amet purus
+        gravida quis. Sed euismod nisi porta lorem mollis aliquam ut. Leo vel
+        orci porta non. Vel orci porta non pulvinar neque laoreet suspendisse
+        interdum consectetur. Scelerisque mauris pellentesque pulvinar
+        pellentesque habitant morbi. Ullamcorper a lacus vestibulum sed arcu non
+        odio euismod. Volutpat sed cras ornare arcu dui. Sit amet est placerat
+        in. Id aliquet risus feugiat in ante metus dictum at.
+      </p>
     </div>
   );
 };
